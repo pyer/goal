57e95fcaf4a0f83501ee525c8e349ee1e11bf441
pyer <pierre.bazonnard@gmail.com>
2026-01-17 17:46:33 +0100

    Convert tabs to spaces

--- v/v.v
+++ v/v.v
@@ -17,43 +17,43 @@ fn show_version() {
 
 
 fn main() {
-	unbuffer_stdout()
-	args := os.args[1..]
+  unbuffer_stdout()
+  args := os.args[1..]
 
-	if args.len == 0 {
+  if args.len == 0 {
     println(v_version)
-		return
-	}
-	prefs := pref.parse_args_and_show_errors()
-	if prefs.show_help {
+    return
+  }
+  prefs := pref.parse_args_and_show_errors()
+  if prefs.show_help {
     show_help()
-		return
-	}
-	if prefs.show_version {
+    return
+  }
+  if prefs.show_version {
     show_version()
-		return
-	}
+    return
+  }
 
-	// Construct the V object from command line arguments
-	mut b := builder.new_builder(prefs)
+  // Construct the V object from command line arguments
+  mut b := builder.new_builder(prefs)
 
-	mut files := b.get_builtin_files()
-	files << b.get_user_files()
-	if b.pref.is_verbose {
-		println('files: ')
-		println(files)
-	}
+  mut files := b.get_builtin_files()
+  files << b.get_user_files()
+  if b.pref.is_verbose {
+    println('files: ')
+    println(files)
+  }
 
-	b.front_stages(files)!
-	b.middle_stages()!
+  b.front_stages(files)!
+  b.middle_stages()!
 
   // Generate C source
-	out_name_c := b.pref.path[..b.pref.path.len - 1] + 'c'
-	source := c.gen(b.parsed_files, mut b.table, b.pref)
-	os.write_file_array(out_name_c, source) or { panic(err) }
+  out_name_c := b.pref.path[..b.pref.path.len - 1] + 'c'
+  source := c.gen(b.parsed_files, mut b.table, b.pref)
+  os.write_file_array(out_name_c, source) or { panic(err) }
 
   // Compile C source
-	compiler.cc(out_name_c, b.pref)
+  compiler.cc(out_name_c, b.pref)
 
-	util.free_caches()
+  util.free_caches()
 }
