dd2282ff88c15d26aef49cf2ef402fce31dd2e85
pyer <pierre.bazonnard@gmail.com>
2026-01-19 20:24:14 +0100

    Add parse_files and parse_imports in the module parser

--- v/v.v
+++ v/v.v
@@ -2,7 +2,15 @@ import os
 import v.pref
 import v.util
 import v.builder
+import v.ast
 import v.parser
+import v.checker
+import v.transformer
+
+import v.markused
+import v.callgraph
+//import v.depgraph
+//import v.dotgraph
 
 import v.gen.c
 import v.compiler
@@ -46,15 +54,47 @@ fn main() {
     println(files)
   }
 
-  //b.front_stages(files)!
-	parsed_files := parser.parse_files(files, mut b.table, prefs)
-	b.parse_imports(parsed_files)
+  // Parse files
+	//parsed_files []&ast.File
+	mut table := ast.new_table()
+	table.is_fmt = false
+	table.pointer_size = if prefs.m64 { 8 } else { 4 }
 
-  b.middle_stages()!
+  println('Parse files')
+	mut parsed_files := parser.parse_files(files, mut table, prefs)
+  println('Parse imports')
+	parser.parse_imports(mut parsed_files, mut table, prefs)
+
+	table.generic_insts_to_concrete()
+
+  println('Check new')
+	mut check := checker.new_checker(table, prefs)
+  println('Check files')
+	check.check_files(parsed_files)
+
+//	b.comptime.solve_files(parsed_files)
+
+	b.print_warnings_and_errors()
+	if check.should_abort {
+		error('too many errors/warnings/notices')
+    return
+	}
+	if check.unresolved_fixed_sizes.len > 0 {
+		check.update_unresolved_fixed_sizes()
+	}
+	mut transf := transformer.new_transformer_with_table(table, prefs)
+	transf.transform_files(parsed_files)
+	table.complete_interface_check()
+	if prefs.skip_unused {
+		markused.mark_used(mut table, prefs, parsed_files)
+	}
+	if prefs.show_callgraph {
+		callgraph.show(mut table, prefs, parsed_files)
+	}
 
   // Generate C source
   out_name_c := prefs.path[..prefs.path.len - 1] + 'c'
-  source := c.gen(b.parsed_files, mut b.table, prefs)
+  source := c.gen(parsed_files, mut table, prefs)
   os.write_file_array(out_name_c, source) or { panic(err) }
 
   // Compile C source
